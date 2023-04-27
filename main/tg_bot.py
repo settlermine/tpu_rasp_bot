ef044a43ff7ccd49405d52a9bdf0c067b469607b
settlermine <settlermine@yandex.ru>
2023-04-27 13:58:13 +0700

    Added proxy to tg_bot.py

--- main/tg_bot.py
+++ main/tg_bot.py
@@ -10,7 +10,10 @@ import datetime
 import pytz
 import asyncio
 import logging
+from telebot import asyncio_helper
 
+#This line can be deleted if it doesn't work with it
+asyncio_helper.proxy = 'http://proxy.server:3128'
 
 class CommandMessages:
     def __new__(cls): pass
